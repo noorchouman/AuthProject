7386be361bce9cf8aede5cf1b893c5e133b5d17e
Noor Chouman <noorchouman123@gmail.com>
2025-07-10 13:51:34 +0300

    unused import

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.example.demo.repo.UserRepository;
 import com.example.demo.service.OtpService;
 import com.example.demo.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
